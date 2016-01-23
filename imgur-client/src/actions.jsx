ca840870d66f7175976d34bc7c1229532fc486d4
davidlygagnon <david.lygagnon@gmail.com>
2016-01-23 01:27:25 -0800

    added image details

--- imgur-client/src/actions.jsx
+++ imgur-client/src/actions.jsx
@@ -3,5 +3,6 @@ var Reflux = require('reflux');
 // Action object, contains only action
 module.exports = Reflux.createActions([
     'getTopics', // single action
-    'getImages'
+    'getImages',
+    'getImage'
 ]);
--- imgur-client/src/components/image-detail.jsx
+++ imgur-client/src/components/image-detail.jsx
@@ -1,9 +1,67 @@
 var React = require('react');
+var Reflux = require('reflux');
+var ImageStore = require('../store/image-store');
+var CommentStore = require('../store/comment-store');
+var Actions = require('../actions');
+var CommentBox = require('./comment-box');
 
 module.exports = React.createClass({
+    mixins: [
+        Reflux.listenTo(ImageStore, 'onChange'),
+        Reflux.listenTo(CommentStore, 'onChange')
+    ],
+    getInitialState: function() {
+        return {
+            image: null,
+            comment: null
+        }
+    },
+    componentWillMount: function() {
+        // this actually calls all store that implements getImage.
+        Actions.getImage(this.props.params.id);
+    },
     render: function () {
-        return <div>
-            I am an image detail.
+        return <div className="image-detail">
+            {this.state.image ? this.renderContent() : null}
         </div>
+    },
+    renderContent: function() {
+        return <div>
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4>{this.state.image.title}</h4>
+                </div>
+                <div className="panel-body">
+                    {this.renderImage()}
+                </div>
+                <div className="panel-footer">
+                    <h5>{this.state.image.description}</h5>
+                </div>
+            </div>
+            <h3>Comments</h3>
+            {this.renderComments()}
+        </div>
+    },
+    renderComments: function() {
+        if (!this.state.comment) {
+            return null
+        }
+
+        return <CommentBox comment={this.state.comment} />
+    },
+    renderImage: function() {
+        if (this.state.image.animated) {
+            return <video preload="auto" autoPlay="autoPlay" loop="loop" webkit-playsinline>
+                <source src={this.state.image.mp4} type="video/mp4"></source>
+            </video>
+        } else {
+            return <img src={this.state.image.link} />
+        }
+    },
+    onChange: function () {
+        this.setState({
+            image: ImageStore.find(this.props.params.id),
+            comment: CommentStore.comment
+        });
     }
-})
+});
